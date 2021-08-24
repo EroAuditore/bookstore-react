cd1eb4c703e1a10999113c941d5196e7cc49b005
eroauditore <alfa.fenrir@gmail.com>
2021-08-24 17:37:54 -0500

    Book item component added

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const Books = () => <div>Not yet implemented</div>;
+const Books = () => {
+  return (
+    <div>
+      <div>
+        <h2>Books</h2>
+        <ul>
+          <BookItem />
+        </ul>
+      </div>
+      <div>
+        <AddBooks />
+      </div>
+    </div>
+  );
+};
 
 export default Books;
