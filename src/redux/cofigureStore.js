4d7b0a6232a40f9e90a58070e3d6007cbfc877b5
eroauditore <alfa.fenrir@gmail.com>
2021-08-25 12:30:14 -0500

    Redux thunk added to store

--- src/redux/cofigureStore.js
+++ src/redux/cofigureStore.js
@@ -1,6 +1,7 @@
 import {
   createStore, combineReducers, applyMiddleware, compose,
 } from 'redux';
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
 
@@ -11,7 +12,10 @@ const reducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-disable no-underscore-dangle */
-const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(logger, thunk)),
+);
 /* eslint-enable */
 
 export default store;
