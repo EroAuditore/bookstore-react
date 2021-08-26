89df8970c407633b135f35aca9db558551dbb5c4
eroauditore <alfa.fenrir@gmail.com>
2021-08-26 00:31:13 -0500

    NavBar styled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,22 @@
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-  <div>
-    <Link to="/">Books</Link>
-    <Link to="/categories">Categories</Link>
+  <div className="panel-bg">
+    <div className="panel-links">
+      <span className="Bookstore-CMS">Bookstore CMS</span>
+      <div className="panel-links-nav">
+        <Link className="BOOKS" to="/">
+          Books
+        </Link>
+        <Link className="CATEGORIES" to="/categories">
+          Categories
+        </Link>
+      </div>
+    </div>
+
+    <div className="Oval">
+      <i className="fas fa-user Mask"> </i>
+    </div>
   </div>
 );
 
